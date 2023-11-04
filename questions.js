1216cc24770e40a1f6e27d0a1235ed03a261c8c6
Prasadrakhi2 <prasadrakhi20004@gmail.com>
2023-11-04 14:47:47 +0530

    Quiz App

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb:1,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        Options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language",
+        ]
+    },
+
+    {
+        numb:2,
+        question: "What does CSS stand for?",
+        answer:  "Cascading style sheet",
+        Options: [
+            "Common Style sheet",
+            "Colorful style sheet",
+            "Coumputer style sheet",
+            "Cascading style sheet",
+        ]
+    },
+
+    {
+        numb:3,
+        question: "What does PHP stand for?",
+        answer:  "HyperText Preprocessor",
+        Options: [
+            "HyperText Preprocessor",
+            "Hypertext programming",
+            "Hypertext Preprogramming",
+            "Hometext preprocessor",
+        ]
+    },
+
+    {
+        numb:4,
+        question: "What does SQL stand for?",
+        answer:  "Structure query language",
+        Options: [
+            "Stylish question language",
+            "Stylesheet query langage",
+            "Statement question language",
+            "Structure query language",
+        ]
+    },
+
+    {
+        numb:5,
+        question: "What does XML stand for?",
+        answer:  "eXtensible markup language",
+        Options: [
+            "eXtensible markup language",
+            "eXtensible multiple language",
+            "eXtra multi-program language",
+            "eXamin multiple language",
+        ]
+    },
+];
